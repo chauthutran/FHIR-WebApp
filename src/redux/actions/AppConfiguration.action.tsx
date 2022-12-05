aeef4335c5470f2858aed3d532d93a45704d954c
chauthutran <cthutran@gmail.com>
2022-12-05 14:54:44 +0900

    Update

--- src/redux/actions/AppConfiguration.action.tsx
+++ src/redux/actions/AppConfiguration.action.tsx
@@ -1,9 +1,8 @@
 import { Action } from "redux";
 import { ThunkAction } from "redux-thunk";
-import * as Types from "../../varTypes"
 import * as Constant from "../../constants";
 import * as api from "../../api";
-import { AppState } from '../../redux/store';
+import { AppDispatch, AppState } from '../../redux/store';
 
 type AppThunk = ThunkAction<
   void,
@@ -12,28 +11,29 @@ type AppThunk = ThunkAction<
   Action<string>
 >;
 
-export const fetchAppConfiguration = (appId: string): AppThunk  =>  async dispatch => {
-    dispatch({
-        type: Constant.FETCH_APP_CONFIG_REQUEST
-    });
-
-    try
-    { 
-        const configData: any = await api.getAppConfiguration( appId );
-        const status: string = ( configData.total > 0 ) ? Constant.FETCH_APP_CONFIG_SUCCESS: Constant.FETCH_APP_CONFIG_FAILURE;
+export function fetchAppConfiguration(appId: string): (dispatch: AppDispatch) => Promise<void> {
+    return async (dispatch: AppDispatch) => {
+   
         dispatch({
-            type: status,
-            payload: configData
-        })
-    }
-    catch(e)
-    {
-        dispatch({
-            type: Constant.FETCH_APP_CONFIG_FAILURE,
-            payload: e
-        })
-    }
-};
+            type: Constant.FETCH_APP_CONFIG_REQUEST
+        });
 
-  
+        try
+        { 
+            const configData: any = await api.getAppConfiguration( appId );
+            const status: string = ( configData.total > 0 ) ? Constant.FETCH_APP_CONFIG_SUCCESS: Constant.FETCH_APP_CONFIG_FAILURE;
+            dispatch({
+                type: status,
+                payload: configData
+            })
+        }
+        catch(e)
+        {
+            dispatch({
+                type: Constant.FETCH_APP_CONFIG_FAILURE,
+                payload: e
+            })
+        }
+    };
+}
 
--- src/redux/actions/ResourceTypes.action.tsx
+++ src/redux/actions/ResourceTypes.action.tsx
@@ -1,11 +1,8 @@
-import { Dispatch } from "redux";
 import { Action } from "redux";
 import { ThunkAction } from "redux-thunk";
 import * as Constant from "../../constants";
 import * as api from "../../api";
 import { AppDispatch, AppState } from '../../redux/store';
-import { ReduxActionType } from "../../varTypes";
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 type AppThunk = ThunkAction<
   void,
@@ -13,24 +10,10 @@ type AppThunk = ThunkAction<
   null,
   Action<string>
 >;
-
-// export const useAppDispatch: () => AppDispatch = useDispatch;
  
 export function fetchResourceTypeList(resourceType: string, searchBy: string, searchValue: string): (dispatch: AppDispatch) => Promise<void> {
     return async (dispatch: AppDispatch) => {
-    //   // Signal work in progress.
-    //   dispatch(signInInProgress());
-  
-    //   try {
-    //     await signInToApi();
-  
-    //     dispatch(signInSuccess());
-    //   } catch (err) {
-    //     dispatch(signInFail(err));
-    //   }
-
-    // const dispatch = useAppDispatch();
-console.log(dispatch);
+   
         dispatch({
             type: Constant.FETCH_RESOURCE_TYPE_REQUEST
         });
@@ -43,17 +26,6 @@ console.log(dispatch);
                 type: status,
                 payload: responseData
             })
-            
-            // dispatch({
-            //     // type: status,
-            //     type: Constant.FETCH_RESOURCE_TYPE_SUCCESS,
-            //     payload: {
-            //         responseData: {
-            //             data:{  entry:[] }
-                        
-            //         }
-            //     }
-            // })
         }
         catch(e)
         {
@@ -63,41 +35,4 @@ console.log(dispatch);
             })
         }
     };
-  }
-
-// export const fetchResourceTypeList = (resourceType: string, searchBy: string, searchValue: string): AppThunk  =>  async dispatch => {
-//     dispatch({
-//         type: Constant.FETCH_RESOURCE_TYPE_REQUEST
-//     });
-
-//     try
-//     { 
-//         const responseData: any = await api.getResourceTypeList( resourceType,searchBy, searchValue );
-//         const status: string = ( responseData.responseData.statusText === "OK" ) ? Constant.FETCH_RESOURCE_TYPE_SUCCESS: Constant.FETCH_RESOURCE_TYPE_FAILURE;
-//         dispatch({
-//             type: status,
-//             payload: responseData
-//         })
-       
-//         // dispatch({
-//         //     // type: status,
-//         //     type: Constant.FETCH_RESOURCE_TYPE_SUCCESS,
-//         //     payload: {
-//         //         responseData: {
-//         //             data:{  entry:[] }
-                   
-//         //         }
-//         //     }
-//         // })
-//     }
-//     catch(e)
-//     {
-//         dispatch({
-//             type: Constant.FETCH_RESOURCE_TYPE_FAILURE,
-//             payload: e
-//         })
-//     }
-// };
-
-  
-
+}
