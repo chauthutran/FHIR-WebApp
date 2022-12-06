464cac64bdf07a60eb28e602c9f65bb50017d7e6
chauthutran <cthutran@gmail.com>
2022-12-06 23:01:54 +0900

    Add styles

--- src/components/Login.component.tsx
+++ src/components/Login.component.tsx
@@ -2,12 +2,13 @@ import React, { FunctionComponent, useEffect, useState } from 'react'; // import
 import Alert, { AlertColor } from '@mui/material/Alert';
 import * as Constant from "../constants";
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import { login, resetMessageStatus, setMessageStatus } from '../redux';
 import { connect } from "react-redux";
 import * as ReduxVarType from "../types";
 import { AppState } from '../redux/store';
 import { Portal, Snackbar } from '@mui/material';
+import "../styles/style.css";
+
 
 type LoginType = {
 	statusData: ReduxVarType.StatusDataType,
@@ -45,31 +46,78 @@ const Login: FunctionComponent<LoginType> = ({ statusData, appConfigData, resour
 	}
 
 	return (
-		<div>
-			
+		<div id="loginFormDiv">
 			{/* {( statusData.status === Constant.FETCH_LOGIN_REQUEST  
 					|| statusData.status === Constant.FETCH_LOGIN_FAILURE 
 					|| statusData.status === Constant.SET_MESSAGE_STATUS ) &&  <Portal> */}
-				{statusData.message != "" &&  <Portal>
-						<Snackbar
-							anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-							open={statusData.message !=""}
-						>
-							<Alert onClose={() => handleCloseAlert()} severity={statusData.type} variant="filled">
-								{statusData.message}
-							</Alert>
-						</Snackbar>
-					</Portal> }
+			{statusData.message != "" &&  <Portal>
+				<Snackbar
+					anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+					open={statusData.message !=""}
+				>
+					<Alert onClose={() => handleCloseAlert()} severity={statusData.type} variant="filled">
+						{statusData.message}
+					</Alert>
+				</Snackbar>
+			</Portal> }
+			
 
-			<div>User code</div>
-			<div><TextField name="name" onChange={(event: any) => setName(event.target.value)} defaultValue="GT2_TEST_IPC" /></div>
+			<div className="wrapper_login">
+				<div className="login_header">
+					<div className="login_logo__image"></div>
+					<div className="login_title">PSI - workforce app</div>
+				</div>
 
-			<div>PIN</div>
-			<div><TextField name="pwd" type="password" onChange={(event: any) => setPassword(event.target.value)} defaultValue="1234" /></div>
+				<div className="login_data">
+					<div className="login_data__fields">
+						<div id="loginField" className="field" style={{backgroundColor: "white", border: "0px" }}>
+							<div className="field__label">
+								<label>User code</label><span>*</span>
+							</div>
+							<div className="field__controls">
+								<div className="field__left" style={{width: "100%"}}>
+									<TextField name="name" onChange={(event: any) => setName(event.target.value)} defaultValue="GT2_TEST_IPC" />
+								</div>
+								<div className="field__right"></div>
+							</div>
+						</div>
+						<div className="field pin">
+							<div className="field__label">
+								<label>PIN</label><span>*</span>
+							</div>
+							<div className="field__controls">
+								<div className="field__left" style={{width: "100%"}}>
+									<TextField name="pwd" type="password" onChange={(event: any) => setPassword(event.target.value)} defaultValue="1234" />
+								</div>
+								<div className="field__right"></div>
+							</div>
+						</div>
+					</div>
+					<div id="divAppVersion" className="login_data__more" style={{textAlign: "left"}}>
+						<div>
+							<label id="spanVersion" style={{color: "#999999", fontWeight: "350"}}>Version 0.1 SNAPSHOT</label>
+							<label id="spanVerDate" style={{marginLeft: "7px", color: "#999999", fontWeight: "350"}}> [2022-12-16]</label>
+						</div>
+					</div>
+				</div>
 
-			<Button variant="contained" onClick={(event: any) => handleOnClick(event)}>Log in</Button>
-
+				<div className="login_buttons login_cta">
+					<div className="login_buttons__container">
+						<div className="button l-emphasis button-full_width mouseDown" id="advanceOptionLoginBtn">
+							<div className="button__container">
+								<div className="button-label">Options</div>
+							</div>
+						</div>
+						<div className="button h-emphasis button-full_width loginBtn mouseDown" >
+							<div className="button__container ">
+								<div className="button-label" onClick={(event: any) => handleOnClick(event)} >LOG IN</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
+		
 	);
 }
 
--- src/components/PatientList.component.tsx
+++ src/components/PatientList.component.tsx
@@ -3,12 +3,33 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+
 import { fetchResourceTypeList, fetchResourceTypeDetails } from '../redux';
 import { connect } from "react-redux";
 import * as ReduxVarType from "../types";
 import * as Constant from "../constants";
 import { AppState } from '../redux/store';
 import * as Utils from "../utils";
+import imgAppDataSyncStatus from "../images/sync-button.svg";
+import imgNetworkStatus from "../images/cloud_online_nav.svg";
+import SyncIcon from '@mui/icons-material/Sync';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 type PatientListType = {
 	statusData: ReduxVarType.StatusDataType,
@@ -20,6 +41,8 @@ type PatientListType = {
 
 const PatientList: FunctionComponent<PatientListType> = ({statusData, resourceTypeList, appConfigData, fetchResourceTypeList, fetchResourceTypeDetails}) => {
 	
+    const [showMenu, setShowMenu] = useState(false);
+
     useEffect(() => {
         console.log(" === appConfigData.loaded : useEffect ");
         if( appConfigData.loaded && resourceTypeList.Patient === undefined )
@@ -36,13 +59,114 @@ const PatientList: FunctionComponent<PatientListType> = ({statusData, resourceTy
         fetchResourceTypeDetails("Patient", id );
     };
 
+    
+  const toggleDrawer =
+    ( open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+        console.log( "toggleDrawer : " + open );
+        if ( event.type === 'keydown' &&  ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift') ) {
+            return;
+        }
+
+        setShowMenu(open);
+    };
+    
+	const renderMenuList = () => {
+        return <Box
+            role="presentation"
+            // onClick={toggleDrawer(false)}
+            // onKeyDown={toggleDrawer(false)}
+            >
+            <List>
+                <ListItem key="clientList" disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <div className="navigation__items-icon" style={{backgroundImage: "url(../images/menu_client_list.svg)" }}></div>
+                        </ListItemIcon>
+                        <ListItemText primary="Client List" />
+                    </ListItemButton>
+                </ListItem>
+
+                <Divider />
+                <ListItem key="recordList" disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <div className="navigation__items-icon" style={{backgroundImage: "url(../images/menu_records_list.svg.svg)" }}></div>
+                        </ListItemIcon>
+                        <ListItemText primary="Record List" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem key="upcomingSchedules" disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <div className="navigation__items-icon" style={{backgroundImage: "url(../images/menu_records_list.svg.svg)" }}></div>
+                        </ListItemIcon>
+                        <ListItemText primary="Upcoming Schedules" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+	}
+
 
 	return ( resourceTypeList.Patient === undefined ) 
         ? <div>Loading client list ...</div> 
     
     : (
 		<div>
-			<div>Client List</div>
+            <AppBar position="static" style={{backgroundColor: "#0D47A1"}}>
+                <Toolbar>
+                    {/* Icon for menu which is showed on the left side */}
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={toggleDrawer(true)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    {/* ==================== */}
+
+                    {/* Menu item */}
+                    <Drawer anchor="left" open={showMenu} onClose={toggleDrawer(false)} >
+                            {renderMenuList()}
+                    </Drawer>
+                    {/* ==================== */}
+
+                    {/* App Title */}
+                    <Typography  component="div" sx={{ flexGrow: 1 }}>
+                        Client List
+                    </Typography>
+                    {/* ==================== */}
+
+                    {/* The right icons */}
+                    <div>
+                        <IconButton
+                            size="large"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            // onClick={}
+                            color="inherit"
+                        >
+                            <SyncIcon />
+                        </IconButton>
+                        
+                        <IconButton
+                            size="large"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            // onClick={}
+                            color="inherit"
+                        >
+                            <CloudUploadIcon />
+                        </IconButton>
+                    
+                    </div>
+                    {/* ==================== */}
+                </Toolbar>
+            </AppBar>
+
 			<div>
                 {resourceTypeList.Patient && <List key="Patient">
                    
